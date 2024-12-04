eee21a8ad45068ff5545c85fac1ad4858284f49e
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2024-12-04 21:56:17 +0100

    search icon + mobile styling

---
+++ static/js/search.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchToggle = document.getElementById("searchToggle");
+    const profileBtn = document.getElementById("profileBtn");
+    const searchForm = document.getElementById("searchForm");
+    const searchInput = document.getElementById("search");
+
+    // Show search field and hide button
+    searchToggle.addEventListener("click", () => {
+        searchForm.classList.add("d-block");
+        searchForm.classList.remove("d-none");
+        profileBtn.classList.add("d-none");
+        profileBtn.classList.remove("d-block");
+        searchToggle.classList.add("d-none");
+        searchInput.focus();
+    });
+
+    // If user search hide search field and show button
+    document.addEventListener("click", (event) => {
+        if (!searchForm.contains(event.target) && !searchToggle.contains(event.target)) {
+            searchForm.classList.add("d-none");
+            searchForm.classList.remove("d-block");
+            searchToggle.classList.remove("d-none");
+        }
+    });
+});
+
