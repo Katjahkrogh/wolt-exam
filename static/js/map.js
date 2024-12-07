71de2c063796782afe00bb1748f205f5a3c64147
Hannah2609 <hann7419@stud.kea.dk>
2024-12-07 21:05:12 +0100

    .

--- static/js/map.js
+++ static/js/map.js
@@ -16,21 +16,19 @@ async function loadRestaurants() {
         data.restaurants.forEach(restaurant => {
             const marker = L.marker([restaurant.latitude, restaurant.longitude]).addTo(map);
             marker.bindPopup(`
-                <article class="w-100% p-10 d-flex flex-col j-content-between overflow-hidden" hover="cursor-pointer scale-101 ts-500">
                 <a href="${restaurant.url}" class="w-100%">
-                    <div class="relative">
+                <article class="w-100% p-8 d-flex flex-col gap-4 j-content-between overflow-hidden" hover="cursor-pointer scale-101 ts-500">
+                    <div>
                         <img src="${restaurant.avatar_url}"
                             alt="${restaurant.name}" 
-                            class="h-30 w-full obj-f-cover">
+                            class="h-30 w-full obj-f-cover rounded-md shadow-md">
                     </div>
-                    <div class="d-flex flex-row j-content-between a-items-center">
-                        <div>
-                            <h3 class="text-c-black mb-0">${restaurant.name}</h3>
-                            <p class="text-c-grey4 text-90 ma-0">${restaurant.address}</p>
-                        </div>
+                    <div>
+                        <h3 class="text-c-black mb-0">${restaurant.name}</h3>
+                        <p class="text-c-primary text-90 mt-0 pa-0">${restaurant.address}</p>
                     </div>
-                </a>
                 </article> 
+                </a>
             `);
         });
     } else {
